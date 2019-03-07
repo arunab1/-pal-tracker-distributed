9f20aa74f401ce40d06c1405f8ed7cd8108c9b83
Raj Jasti <raj.jasti@gmail.com>
2019-03-07 09:45:38 -0700

    eureka server changes

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -21,7 +21,7 @@ public class ApplicationServer {
     }
 
 
-    public void start(Map<String, String> env) throws IOException, InterruptedException {
+    private void start(Map<String, String> env) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder()
             .command("java", "-jar", jarPath)
             .inheritIO();
@@ -37,7 +37,9 @@ public class ApplicationServer {
 
         start(envMapBuilder()
             .put("SPRING_DATASOURCE_URL", dbUrl)
-            .put("REGISTRATION_SERVER_ENDPOINT", "http://localhost:8883")
+            .put("EUREKA_CLIENT_ENABLED", "false")
+            .put("RIBBON_EUREKA_ENABLED", "false")
+            .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
             .build()
         );
     }
